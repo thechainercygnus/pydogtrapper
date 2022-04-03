a90f4f73cabd247ad305b05e21da15058820da60
Bryce <bryce@durish.xyz>
2022-04-03 14:58:32 -0600

    Added stress functionality

--- animals/_animals.py
+++ animals/_animals.py
@@ -1,3 +1,11 @@
+from typing import NamedTuple
+
+
+class fear_state(NamedTuple):
+    stress_trigger: int
+    state: str
+
+
 STATES = ("normal", "alert", "frightened", "fear", "terror")
 
 
@@ -10,6 +18,7 @@ class Animal:
         self.species = species
         self._state = state or 0
         self._injuries = injuries
+        self._stress_level = self._state * 10 if self._state > 0 else 0
 
     def __str__(self):
         return f"<Animal(Species={self.species})>"
@@ -17,14 +26,33 @@ class Animal:
     def __repr__(self):
         return f"<Animal#{self.__ordinal}>"
 
+    def add_stress(self, stress: int) -> None:
+        self._stress_level += stress
+        self._set_state()
+
+    def remove_stress(self, stress: int) -> None:
+        self._stress_level -= stress
+        self._set_state()
+
     @property
-    def ordinal(self):
+    def ordinal(self) -> int:
         return self.__ordinal
 
     @property
-    def state(self):
+    def state(self) -> str:
         return STATES[self._state]
 
     @property
-    def injuries(self):
+    def injuries(self) -> dict:
         return self._injuries
+
+    @property
+    def stress_level(self) -> int:
+        return self._stress_level
+
+    def _set_state(self) -> None:
+        _state = int(round(self.stress_level / 10, 0))
+        if _state > len(STATES) - 1:
+            self._state = len(STATES) - 1
+        if _state < 0:
+            self._state = 0
--- tests/test_animals.py
+++ tests/test_animals.py
@@ -13,6 +13,11 @@ def base_dog():
     return Dog(name="Frankie")
 
 
+@pytest.fixture
+def injured_dog():
+    return Dog(name="Frankie", state=1, injuries={"ear": {"left": "nicked"}})
+
+
 def test_should_create_base_animal(base_animal):
     assert base_animal is not None
 
@@ -52,3 +57,42 @@ def test_should_create_dog_in_non_default_state_with_injuries():
     injured_dog = Dog(name="Frankie", state=2, injuries={"ear": {"left": "nicked"}})
     assert injured_dog.state == "frightened"
     assert injured_dog.injuries == {"ear": {"left": "nicked"}}
+
+
+def test_should_report_dogs_stress_level(base_dog):
+    assert base_dog.stress_level == 0
+
+
+def test_should_increase_dogs_state_by_adding_stress(base_dog):
+    assert base_dog.stress_level == 0
+    assert base_dog.state == STATES[0]
+
+    base_dog.add_stress(10)
+
+    assert base_dog.stress_level == 10
+    assert base_dog.state == STATES[1]
+
+
+def test_should_decrease_dogs_state_by_subtracting_stress(injured_dog):
+    assert injured_dog.stress_level == 10
+    assert injured_dog.state == STATES[1]
+
+    injured_dog.remove_stress(10)
+
+    assert injured_dog.stress_level == 0
+    assert injured_dog.state == STATES[0]
+
+
+def test_should_catch_state_out_of_bounds(base_dog):
+    assert base_dog.stress_level == 0
+    assert base_dog.state == STATES[0]
+
+    base_dog.add_stress(1000)
+
+    assert base_dog.stress_level == 1000
+    assert base_dog.state == STATES[-1]
+
+    base_dog.remove_stress(2000)
+
+    assert base_dog.stress_level == -1000
+    assert base_dog.state == STATES[0]
